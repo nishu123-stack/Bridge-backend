0fca4a552d4aa996ccdfa62cad5e5e6f0f59c70e
Nishchith Shetty <nishchithshetty99@gmail.com>
2020-09-08 11:25:12 +0530

    editing profile

---
+++ functions/routes/profile/editProfile.js
@@ -0,0 +1,28 @@
+const profileRouter = require('express').Router();
+const firebase = require('firebase');
+const db = require('../../auth/app');
+
+
+const middleware = require('../../auth/authorization');
+
+profileRouter.get('/',middleware.checkToken,middleware.authorizeToken, async ( req, res, next) =>{
+    try {
+        const docRef = await db.collection('users').doc(req.uid);
+        const resData = await docRef.get();
+        // if(!resData.exists){
+        //    return res.status(204).send('No user found');
+        // }
+        // else {
+        //     console.log('Document data:', doc.data());
+        // }
+        
+
+        return res.status(200).send(resData.data());
+        
+    } catch (err) {
+        return res.send(err);
+    }
+
+});
+
+module.exports = profileRouter
